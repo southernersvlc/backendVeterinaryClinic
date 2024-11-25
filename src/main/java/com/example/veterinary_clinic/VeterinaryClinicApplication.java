f70c13bbfd7c2e2a5a9dfa5b11532b16626af43a
Paola Perdomo <paolaperdomo079@gmail.com>
2024-11-25 14:47:02 +0100

    feat: Coneccion to database done and table owner created

--- src/main/java/com/example/veterinary_clinic/VeterinaryClinicApplication.java
+++ src/main/java/com/example/veterinary_clinic/VeterinaryClinicApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VeterinaryClinicApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VeterinaryClinicApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
