250f4af845a87fe406805ed5148eb0cd2abe4f08
Sergi <sergi.prof@hotmail.com>
2024-12-12 10:41:32 +0100

    feat: add PetServicesTest

---
+++ src/test/java/com/example/veterinary_clinic/services/PetServicesTest.java
@@ -0,0 +1,12 @@
+package com.example.veterinary_clinic.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetServicesTest {
+
+    @Test
+    void createPet() {
+    }
+}
