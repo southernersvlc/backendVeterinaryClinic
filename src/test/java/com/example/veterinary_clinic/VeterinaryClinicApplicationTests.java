aa249cfc0f9014a738afa365429299b54709f7ac
Sergi <sergi.prof@hotmail.com>
2024-12-13 13:50:23 +0100

    fix: check pipeline and changes @ActiveProfile in VetClinicAppTest

--- src/test/java/com/example/veterinary_clinic/VeterinaryClinicApplicationTests.java
+++ src/test/java/com/example/veterinary_clinic/VeterinaryClinicApplicationTests.java
@@ -2,7 +2,10 @@ package com.example.veterinary_clinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class VeterinaryClinicApplicationTests {
 
