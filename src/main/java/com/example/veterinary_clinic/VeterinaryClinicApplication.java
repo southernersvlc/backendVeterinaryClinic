35c10ed1506a04be6ef89daf8fae0a32f87dddbd
Paola Perdomo <paolaperdomo079@gmail.com>
2024-12-15 17:58:49 +0100

    comented new lines added because pipeline failed

--- src/main/java/com/example/veterinary_clinic/VeterinaryClinicApplication.java
+++ src/main/java/com/example/veterinary_clinic/VeterinaryClinicApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 public class VeterinaryClinicApplication {
 
-	static {
+	/*static {
 		Dotenv dotenv = Dotenv.configure()
 				.directory("./")
 				.ignoreIfMissing()
@@ -18,7 +18,7 @@ public class VeterinaryClinicApplication {
 		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		System.setProperty("SPRING_DATASOURCE_DRIVERCLASSNAME", dotenv.get("SPRING_DATASOURCE_DRIVERCLASSNAME"));
-	}
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(VeterinaryClinicApplication.class, args);
