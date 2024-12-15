395830e0b4fd98d1bb46d1d23cd3e1530df9fd2a
monica <mosice@gmail.com>
2024-12-15 19:26:24 +0100

    config: add data fake initPetData and  initDataAppointment in InteFakeData

--- src/main/java/com/example/veterinary_clinic/config/InitFakeData.java
+++ src/main/java/com/example/veterinary_clinic/config/InitFakeData.java
@@ -1,25 +1,51 @@
 package com.example.veterinary_clinic.config;
 
+import com.example.veterinary_clinic.entities.Appointment;
 import com.example.veterinary_clinic.entities.Guardian;
+import com.example.veterinary_clinic.entities.Pet;
+import com.example.veterinary_clinic.repositories.AppointmentRepository;
 import com.example.veterinary_clinic.repositories.GuardianRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.veterinary_clinic.repositories.PetRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Configuration
 public class InitFakeData {
 
     @Bean
-    public CommandLineRunner initData(GuardianRepository guardianRepository) {
+    public CommandLineRunner initGuardianData(GuardianRepository guardianRepository) {
         return args -> {
             List<Guardian> guardianList = List.of
                     (new Guardian("Name1", "123456454", "test addres", "hola@email.com"),
                             new Guardian("Name2", "123451454", "test addres 2", "hola@email.com"));
 
             guardianRepository.saveAll(guardianList);
+        };
+    }
+
+    @Bean
+    public CommandLineRunner initPetData(PetRepository petRepository, GuardianRepository guardianRepository) {
+        return args -> {
+            List<Pet> petList = List.of
+                    (new Pet("Pet1", "breed1", "DOG", "3", guardianRepository.getReferenceById(1L)),
+                            new Pet("Pet2", "breed2", "CAT", "5", guardianRepository.getReferenceById(2L)));
+
+            petRepository.saveAll(petList);
+        };
+    }
+
+    @Bean
+    public CommandLineRunner initAppointmentData(PetRepository petRepository, GuardianRepository guardianRepository, AppointmentRepository appointmentRepository) {
+        return args -> {
+            List<Appointment> appointmentList = List.of
+                    (new Appointment(LocalDate.of(2024, 12, 17), LocalTime.of(15, 30), "reason1", petRepository.getReferenceById(1L)));
+
+            appointmentRepository.saveAll(appointmentList);
         };
     }
 }
